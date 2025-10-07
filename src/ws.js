3038eed1c5acf128c13f98beb208bea9af75994d
varshneybhavya <bhavyavarshney10@gmail.com>
2025-10-07 18:03:54 +0530

    changes

--- src/ws.js
+++ src/ws.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
 export function connectWS() {
-    return io('http://localhost:4600');
+    return io('https://uncomplex-pamula-prorealistic.ngrok-free.dev');
 }
